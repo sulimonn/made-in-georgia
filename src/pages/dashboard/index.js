c4bc7fc8dd1773545764e8508cb273c7b87d058e
sulimonn <gsuli836@gmail.com>
2024-04-06 21:34:39 +0600

    first commit

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,34 @@
+// material-ui
+import { Grid, Typography } from '@mui/material';
+
+// project import
+import AnalyticEcommerce from 'components/cards/statistics/AnalyticEcommerce';
+
+// ==============================|| DASHBOARD - DEFAULT ||============================== //
+
+const DashboardDefault = () => {
+  return (
+    <Grid container rowSpacing={4.5} columnSpacing={2.75}>
+      {/* row 1 */}
+      <Grid item xs={12} sx={{ mb: -2.25 }}>
+        <Typography variant="h5">Dashboard</Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <AnalyticEcommerce title="Total Page Views" count="4,42,236" percentage={59.3} extra="35,000" />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <AnalyticEcommerce title="Total Users" count="78,250" percentage={70.5} extra="8,900" />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <AnalyticEcommerce title="Total Order" count="18,800" percentage={27.4} isLoss color="warning" extra="1,943" />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <AnalyticEcommerce title="Total Sales" count="$35,078" percentage={27.4} isLoss color="warning" extra="$20,395" />
+      </Grid>
+
+      <Grid item md={8} sx={{ display: { sm: 'none', md: 'block', lg: 'none' } }} />
+    </Grid>
+  );
+};
+
+export default DashboardDefault;
