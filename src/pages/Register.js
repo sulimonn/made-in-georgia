ec9e090d930731911a2fee63a059ac0538baa818
sulimonn <gsuli836@gmail.com>
2024-05-16 21:36:10 +0600

    input fix

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -53,50 +53,43 @@ const Register = ({ handleClose }) => {
   const handleChange = (e) => {
     const { value, name } = e.target;
 
-    if (value.length === 0) {
-      return;
-    } else {
-      if (name === 'phone') {
-        const lettersOnly = value.replace(/\d/g, '');
-        if (lettersOnly.length > 0 && !lettersOnly.includes('+')) {
-          return;
-        }
-        const inputValue = value.replace(/\D/g, '');
-
-        const numericValue = inputValue.replace(/\D/g, '');
-        let formattedValue = '';
-        if (numericValue.length === 1) {
-          if (input?.phone?.length === 0) {
-            formattedValue = '+7 (' + numericValue;
-          }
-        } else if (numericValue.length > 1) {
-          formattedValue =
-            '+7 (' +
-            numericValue
-              .slice(1, 11)
-              .replace(
-                /(\d{1,3})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/,
-                function (match, p1, p2, p3, p4) {
-                  let formattedNumber = '';
-                  if (p1) formattedNumber += p1 + (p1.length === 3 ? ') ' : '');
-                  if (p2) formattedNumber += ' ' + p2;
-                  if (p3) formattedNumber += '-' + p3;
-                  if (p4) formattedNumber += '-' + p4;
-                  if (
-                    (formattedNumber.length === 5 && input.phone.length === 11) ||
-                    (formattedNumber.length === 5 && input.phone.length === 9)
-                  ) {
-                    formattedNumber = formattedNumber.slice(0, 3);
-                  }
-
-                  return formattedNumber;
-                },
-              );
-        }
-        setInput({ ...input, [name]: formattedValue });
-      } else {
-        setInput({ ...input, [name]: value });
+    if (name === 'phone') {
+      const lettersOnly = value.replace(/\d/g, '');
+      if (lettersOnly.length > 0 && !lettersOnly.includes('+')) {
+        return;
       }
+      const inputValue = value.replace(/\D/g, '');
+
+      const numericValue = inputValue.replace(/\D/g, '');
+      let formattedValue = '';
+      if (numericValue.length === 1) {
+        if (input?.phone?.length === 0) {
+          formattedValue = '+7 (' + numericValue;
+        }
+      } else if (numericValue.length > 1) {
+        formattedValue =
+          '+7 (' +
+          numericValue
+            .slice(1, 11)
+            .replace(/(\d{1,3})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/, function (match, p1, p2, p3, p4) {
+              let formattedNumber = '';
+              if (p1) formattedNumber += p1 + (p1.length === 3 ? ') ' : '');
+              if (p2) formattedNumber += ' ' + p2;
+              if (p3) formattedNumber += '-' + p3;
+              if (p4) formattedNumber += '-' + p4;
+              if (
+                (formattedNumber.length === 5 && input.phone.length === 11) ||
+                (formattedNumber.length === 5 && input.phone.length === 9)
+              ) {
+                formattedNumber = formattedNumber.slice(0, 3);
+              }
+
+              return formattedNumber;
+            });
+      }
+      setInput({ ...input, [name]: formattedValue });
+    } else {
+      setInput({ ...input, [name]: value });
     }
 
     setErrors({ ...errors, [name]: '' });
