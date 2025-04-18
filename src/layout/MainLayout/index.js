bd04f1315cfe567d02927efc265321e4c733c65d
Sulaiman Sadykov <49269100+sulimonn@users.noreply.github.com>
2025-04-18 13:46:38 +0600

    Update index.js

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -37,7 +37,7 @@ const MainLayout = () => {
 
   const { pathname } = useLocation();
 
-  window.location.replace('https://madeingeorgia.ru');
+
   useEffect(() => {
     if (pathname === '/feedback') {
       dispatch(activeItem({ openItem: 'feedback' }));
